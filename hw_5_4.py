0662fd0245e264e5f0b3f7e6f21de55d368ac7b4
Vladyslav Sokolov <wladyslawsokolow@gmail.com>
2025-02-12 20:21:12 +0100

    Add files via upload

---
+++ hw_5_4.py
@@ -0,0 +1,77 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact does not exist."
+        except IndexError:
+            return "Please provide the correct number of arguments."
+    return inner
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+@input_error
+def add_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError
+    name, phone = args
+    if name in contacts:
+        raise KeyError
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError
+    name, phone = args
+    if name not in contacts:
+        raise KeyError
+    contacts[name] = phone
+    return "Contact updated."
+
+@input_error
+def get_phone(args, contacts):
+    if len(args) != 1:
+        raise ValueError
+    name = args[0]
+    if name not in contacts:
+        raise KeyError
+    return f"{name}: {contacts[name]}"
+
+@input_error
+def show_all_contacts(contacts):
+    if not contacts:
+        raise ValueError
+    return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(get_phone(args, contacts))
+        elif command == "all":
+            print(show_all_contacts(contacts))
+        else:
+            print("Invalid command. Please try again.")
+
+if __name__ == "__main__":
+    main()
